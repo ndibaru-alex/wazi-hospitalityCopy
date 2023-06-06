01ade7dac478cd03cc460379aa1f93432de15813
uhuru-rawlings <uhururawlings40@gmail.com>
2023-06-06 09:18:55 +0300

    added page titles

--- cleaning-solutions.php
+++ cleaning-solutions.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="Wazi Hospitality">
     <meta name="author" content="#">
-    <title>Wazi Hospitality</title>
+    <title>Wazi Hospitality | Cleaning Solutions</title>
 
     <!-- Favicons-->
     <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
