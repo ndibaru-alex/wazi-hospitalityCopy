bc22d285943c2c755d250134a0710162259de726
Julius26 <juliusmeldrin@gmail.com>
2023-06-29 15:30:25 +0300

    updates: homepage

--- admin/config.php
+++ admin/config.php
@@ -4,7 +4,7 @@
     ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
     if($_SERVER['HTTP_HOST'] == "127.0.0.1" || $_SERVER['HTTP_HOST'] == "localhost"){
         // define('BASE_URL', 'http://127.0.0.1/Fourtech/Wazi/admin/');
-        define('BASE_URL', 'http://localhost/server/fourtech/wazi/');
+        define('BASE_URL', 'http://localhost/server/fourtech/wazi/admin/');
     }else{
         define('BASE_URL', 'https://www.admin.techrollblogs.com/');
     }
--- index.php
+++ index.php
@@ -1,63 +1,84 @@
 <?php
-	session_start();
-	$_SESSION['active'] = "home"
+session_start();
+$_SESSION['active'] = "home"
 ?>
 <!DOCTYPE html>
 <html lang="en">
 
 
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Wazi Hospitality">
-    <meta name="author" content="#">
-    <title>Wazi Hospitality | Home</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="Wazi Hospitality">
+	<meta name="author" content="#">
+	<title>Wazi Hospitality | Home</title>
 
-    <!-- Favicons-->
-    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
-    <link rel="apple-touch-icon" type="image/x-icon" href="img/apple-touch-icon-57x57-precomposed.png">
-    <link rel="apple-touch-icon" type="image/x-icon" sizes="72x72" href="img/apple-touch-icon-72x72-precomposed.png">
-    <link rel="apple-touch-icon" type="image/x-icon" sizes="114x114" href="img/apple-touch-icon-114x114-precomposed.png">
-    <link rel="apple-touch-icon" type="image/x-icon" sizes="144x144" href="img/apple-touch-icon-144x144-precomposed.png">
+	<!-- Favicons-->
+	<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+	<link rel="apple-touch-icon" type="image/x-icon" href="img/apple-touch-icon-57x57-precomposed.png">
+	<link rel="apple-touch-icon" type="image/x-icon" sizes="72x72" href="img/apple-touch-icon-72x72-precomposed.png">
+	<link rel="apple-touch-icon" type="image/x-icon" sizes="114x114" href="img/apple-touch-icon-114x114-precomposed.png">
+	<link rel="apple-touch-icon" type="image/x-icon" sizes="144x144" href="img/apple-touch-icon-144x144-precomposed.png">
 
-    <!-- GOOGLE WEB FONT -->
-    <link rel="preconnect" href="https://fonts.googleapis.com/">
+	<!-- GOOGLE WEB FONT -->
+	<link rel="preconnect" href="https://fonts.googleapis.com/">
 	<link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Lora:ital@1&amp;family=Poppins:wght@400;500;600;700&amp;display=swap" rel="stylesheet">
 
-    <!-- BASE CSS -->
-    <link href="css/vendors.min.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
+	<!-- BASE CSS -->
+	<link href="css/vendors.min.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
 
-    <!-- SPECIFIC CSS -->
-    <link href="css/wizard.css" rel="stylesheet">
+	<!-- SPECIFIC CSS -->
+	<link href="css/wizard.css" rel="stylesheet">
 
-    <!-- YOUR CUSTOM CSS -->
-    <link href="css/custom.css" rel="stylesheet">
+	<!-- YOUR CUSTOM CSS -->
+	<link href="css/custom.css" rel="stylesheet">
 
 </head>
 
 <body>
 
 	<?php
-		include_once("includes/topNav.php");
+	include_once("includes/topNav.php");
 	?>
-	
+
 	<main>
 		<div id="carousel-home">
 			<div class="owl-carousel owl-theme">
-				<div class="owl-slide cover lazy" data-bg="url(assets/imgs/lunch-and-dinner/green-plate-with-homemade-chocolate-cupcakes.jpg)">
+
+
+			<div class="owl-slide cover lazy" data-bg="url(img/slides/slider-c.jpg)">
+					<div class="opacity-mask d-flex align-items-center" data-opacity-mask="rgba(0, 0, 0, 0.5)">
+						<div class="container">
+							<div class="row justify-content-center justify-content-md-start">
+								<div class="col-lg-6 static">
+									<div class="slide-text white">
+										<h2 class="owl-slide-animated owl-slide-title">
+											Welcome To Wazi Cuisine & Hospitality
+										</h2>
+										<p class="owl-slide-animated owl-slide-subtitle"> Extravagant tastes. Promoting wellness. Relaxing pleasure. </p>
+										<div class="owl-slide-animated owl-slide-cta"><a class="btn_1" href="contacts.php" role="button">Let's Talk</a></div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+
+				<div class="owl-slide cover lazy" data-bg="url(img/slides/slider-b.jpg)">
 					<div class="opacity-mask d-flex align-items-center" data-opacity-mask="rgba(0, 0, 0, 0.5)">
 						<div class="container">
 							<div class="row justify-content-center justify-content-md-end">
 								<div class="col-lg-6 static">
 									<div class="slide-text text-end white">
 										<h2 class="owl-slide-animated owl-slide-title">
-											Discover the Essence of Wazi Cuisine
+											Delivering Excellence
 										</h2>
 										<p class="owl-slide-animated owl-slide-subtitle">
-											Unleashing the Flavors of Hospitality
+											Join a proven journey of value creation
 										</p>
 										<div class="owl-slide-animated owl-slide-cta"><a class="btn_1 btn_scroll" href="about.php" role="button">Read more</a></div>
 									</div>
@@ -66,18 +87,19 @@
 						</div>
 					</div>
 				</div>
+
 				<!--/owl-slide-->
-				<div class="owl-slide cover lazy" data-bg="url(assets/imgs/lunch-and-dinner/buggers.jpg)">
+				<div class="owl-slide cover lazy" data-bg="url(img/slides/slider-c.jpg)">
 					<div class="opacity-mask d-flex align-items-center" data-opacity-mask="rgba(0, 0, 0, 0.5)">
 						<div class="container">
 							<div class="row justify-content-center justify-content-md-start">
 								<div class="col-lg-6 static">
 									<div class="slide-text white">
 										<h2 class="owl-slide-animated owl-slide-title">
-											Empowering Dreams through World-Class Hospitality
+											Empowering dreams
 										</h2>
 										<p class="owl-slide-animated owl-slide-subtitle">
-											Your Pathway to Excellence
+											Enjoy world class hospitality services
 										</p>
 										<div class="owl-slide-animated owl-slide-cta"><a class="btn_1" href="contacts.php" role="button">Let's Talk</a></div>
 									</div>
@@ -86,119 +108,89 @@
 						</div>
 					</div>
 				</div>
-				<!--/owl-slide-->				
-				<div class="owl-slide cover lazy" data-bg="url(assets/imgs/lunch-and-dinner/fish.jpg)">
-					<div class="opacity-mask d-flex align-items-center" data-opacity-mask="rgba(0, 0, 0, 0.6)">
-						<div class="container">
-							<div class="row justify-content-center justify-content-md-start">
-								<div class="col-lg-12 static">
-									<div class="slide-text text-center white">
-										<h2 class="owl-slide-animated owl-slide-title">Experience Excellence in Service</h2>
-										<p class="owl-slide-animated owl-slide-subtitle">
-											Transforming Lives and Creating Lasting Impressions
-										</p>
-										<div class="owl-slide-animated owl-slide-cta"><a class="btn_1" href="menu-1.html" role="button">Let's Talk</a></div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<!--/owl-slide-->
-				</div>
+				<!--/owl-slide-->
+				
 			</div>
 			<div id="icon_drag_mobile"></div>
 		</div>
-		<!--/carousel-->
-
-		<!-- <ul id="banners_grid" class="clearfix">
-			<li>
-				<a href="menu-1.html" class="img_container">
-					<img src="img/banners_cat_placeholder.jpg" data-src="img/banner_1.jpg" alt="" class="lazy">
-					<div class="short_info opacity-mask" data-opacity-mask="rgba(0, 0, 0, 0.5)">
-						<h3>Our Menu</h3>
-						<p>View Our Specialites</p>
-					</div>
-				</a>
-			</li>
-			<li>
-				<a href="order-food.html" class="img_container">
-					<img src="img/banners_cat_placeholder.jpg" data-src="img/banner_2.jpg" alt="" class="lazy">
-					<div class="short_info opacity-mask" data-opacity-mask="rgba(0, 0, 0, 0.5)">
-						<h3>Delivery</h3>
-						<p>Home delivery or take away food</p>
-					</div>
-				</a>
-			</li>
-			<li>
-				<a href="gallery.html" class="img_container">
-					<img src="img/banners_cat_placeholder.jpg" data-src="img/banner_3.jpg" alt="" class="lazy">
-					<div class="short_info opacity-mask" data-opacity-mask="rgba(0, 0, 0, 0.5)">
-						<h3>Inside Foores</h3>
-						<p>View the Gallery</p>
-					</div>
-				</a>
-			</li>
-		</ul> -->
-		<!--/banners_grid -->
+		
 
 		<div class="pattern_2">
-		    <div class="container margin_120_100 home_intro">
-		        <div class="row justify-content-center d-flex align-items-center">
-		            <div class="col-lg-5 text-lg-center d-none d-lg-block" data-cue="slideInUp">
-		                <figure>
-		                    <img src="assets/imgs/restaurant.jpg" data-src="assets/imgs/restaurant.jpg" width="354" height="440" alt="" class="img-fluid lazy">
-		                    <!-- <a href="" class="btn_play" data-cue="zoomIn" data-delay="500"><span class="pulse_bt"><i class="arrow_triangle-right"></i></span></a> -->
-		                </figure>
-		            </div>
-		            <div class="col-lg-5 pt-lg-4" data-cue="slideInUp" data-delay="500">
-		                <div class="main_title">
-		                    <span><em></em></span>
-		                    <h2>About us.</h2>
-		                    <p>Wazi cuisine. Hospitality services provider</p>
-		                </div>
+			<div class="container margin_120_100 home_intro">
+				<div class="row justify-content-center d-flex align-items-center">
+					<div class="col-lg-5 text-lg-center d-none d-lg-block" data-cue="slideInUp">
+						<figure>
+							<img src="assets/imgs/restaurant.jpg" data-src="assets/imgs/restaurant.jpg" width="354" height="440" alt="" class="img-fluid lazy">
+							<!-- <a href="" class="btn_play" data-cue="zoomIn" data-delay="500"><span class="pulse_bt"><i class="arrow_triangle-right"></i></span></a> -->
+						</figure>
+					</div>
+					<div class="col-lg-5 pt-lg-4" data-cue="slideInUp" data-delay="500">
+						<div class="main_title">
+							<span><em></em></span>
+							<h2>About us.</h2>
+							<p>Wazi Cuisine & Hospitality.</p>
+						</div>
 						<p>
-							A leading integrated world class service provider, purpose-led organization, with all our efforts centered on helping our clients live their dream.
+						We are a leading integrated world class service provider, purpose-led and driven organization, with all our efforts centered on helping our clients live their dream through the provision of hospitality services and exceed their expectations.
 						</p>
 						<p>
-							We have a proven track record of delivering superior value to clients, employees, shareholders and broader society.
+						Our strategy is executed through business clusters that deliver tailored, comprehensive and client-centric solutions to individual and institutional clients.
 						</p>
 						<p>
-							Our strategy is executed through business clusters that deliver tailored, comprehensive and client-centric solutions to individual and institutional clients
+						We develop tailored, appropriate solutions and enable our clients to make sound choices. 
 						</p>
-		            </div>
-		        </div>
-		        <!--/row -->
-		    </div>
-		    <!--/container -->
+						<div class=""><a class="btn_1 btn_scroll " href="about.php" role="button">Know us better</a></div>
+					</div>
+				</div>
+				<!--/row -->
+			</div>
+			<!--/container -->
 		</div>
 		<!--/pattern_2 -->
 
 		<div class="bg_gray">
-		    <div class="container margin_120_100" data-cue="slideInUp">
-		        <div class="main_title center mb-5">
-		            <span><em></em></span>
-		            <h2>Our Core Values</h2>
-		        </div>
-		        <!-- /main_title -->
-		        <div class="banner lazy" data-bg="url(assets/imgs/breakfast-options/pumpkin-soup.jpg)">
-   
+			<div class="container margin_120_100" data-cue="slideInUp">
+				<div class="main_title center mb-5">
+					<span><em></em></span>
+					<h2>What Fuels Wazi</h2>
+				</div>
+				<!-- /main_title -->
+				<div class="banner lazy" data-bg="url(assets/imgs/breakfast-options/pumpkin-soup.jpg)">
+
 					<div class="wrapper d-flex align-items-center justify-content-between opacity-mask" data-opacity-mask="rgba(0, 0, 0, 0.5)">
-                        <p>
-							Our core values make us who we are. As we change and grow, the beliefs that are most important to us stay the same. Being part of Wazi Hospitality means being part of a proud history and a thriving culture.
+						<p>
+						We are driven by our purpose of creating unique distinct and sterling(priceless) experiences where and when the client needs them.
 						</p>
-                    </div>
-                    <!-- /wrapper -->
-                </div>
-                <!-- /banner -->
-		        <div class="row magnific-gallery homepage add_bottom_25">
+					</div>
+					<!-- /wrapper -->
+				</div>
+
+
+				<!-- /banner -->
+				<div class="row magnific-gallery homepage add_bottom_25">
 					<div class="main_title center mb-5">
 						<span><em></em></span>
 						<h2>Our Solutions</h2>
 					</div>
+
+
 					<div class="col-sm-4 mb-4">
 						<div class="card border-0 h-100">
 							<div class="card-body">
-								<h4>Cleaning Solutions</h4>
+								<h4>Corporate & Private Catering</h4>
+								<p>
+									Experience the convenience of professional food services tailored to your unique event, with delectable menus, impeccable presentation, and flawless execution that leave a lasting impression.
+								</p>
+								<div class="owl-slide-animated owl-slide-cta"><a class="btn_1 btn_scroll remove_event_listerner" href="catering.php" role="button">Read more</a></div>
+							</div>
+						</div>
+					</div>
+
+
+					<div class="col-sm-4 mb-4">
+						<div class="card border-0 h-100">
+							<div class="card-body">
+								<h4>Professional Cleaning Solutions</h4>
 								<p>
 									We understand first impressions count. Whether you are hosting clients, interviewing, or it’s just the regular workforce, great sanitation and ambiance are critical.
 								</p>
@@ -206,6 +198,8 @@
 							</div>
 						</div>
 					</div>
+
+
 					<div class="col-sm-4 mb-4">
 						<div class="card border-0 h-100">
 							<div class="card-body">
@@ -217,6 +211,7 @@
 							</div>
 						</div>
 					</div>
+
 					<div class="col-sm-4 mb-4">
 						<div class="card border-0 h-100">
 							<div class="card-body">
@@ -228,7 +223,9 @@
 							</div>
 						</div>
 					</div>
-					<div class="col-sm-4 mb-4">
+
+
+					<!-- <div class="col-sm-4 mb-4">
 						<div class="card border-0 h-100">
 							<div class="card-body">
 								<h4>Cuisine</h4>
@@ -238,19 +235,13 @@
 								<div class="owl-slide-animated owl-slide-cta"><a class="btn_1 btn_scroll remove_event_listerner" href="cuisine.php" role="button">Read more</a></div>
 							</div>
 						</div>
-					</div>
-					<div class="col-sm-4 mb-4">
-						<div class="card border-0 h-100">
-							<div class="card-body">
-								<h4>Catering</h4>
-								<p>
-									Experience the convenience of professional food services tailored to your unique event, with delectable menus, impeccable presentation, and flawless execution that leave a lasting impression.
-								</p>
-								<div class="owl-slide-animated owl-slide-cta"><a class="btn_1 btn_scroll remove_event_listerner" href="catering.php" role="button">Read more</a></div>
-							</div>
-						</div>
-					</div>
-					<div class="col-sm-4 mb-4">
+					</div> -->
+
+
+					
+
+
+					<!-- <div class="col-sm-4 mb-4">
 						<div class="card border-0 h-100">
 							<div class="card-body">
 								<h4>Pest Control</h4>
@@ -260,8 +251,10 @@
 								<div class="owl-slide-animated owl-slide-cta"><a class="btn_1 btn_scroll remove_event_listerner" href="pest-control-solutions.php" role="button">Read more</a></div>
 							</div>
 						</div>
-					</div>
-					<div class="col-sm-4 mb-4">
+					</div> -->
+
+
+					<!-- <div class="col-sm-4 mb-4">
 						<div class="card border-0 h-100">
 							<div class="card-body">
 								<h4>Kitchen and Restaurant Design</h4>
@@ -271,37 +264,39 @@
 								<div class="owl-slide-animated owl-slide-cta"><a class="btn_1 btn_scroll remove_event_listerner" href="kitchen-and-restaurant-design.php" role="button">Read more</a></div>
 							</div>
 						</div>
-					</div>
-                </div>
-                <!-- /row -->
-		    </div>
-		    <!-- /container -->
+					</div> -->
+
+
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
 		</div>
 		<!-- /bg_gray -->
 
 		<div class="call_section lazy" data-bg="url(assets/imgs/breakfast-options/chaitea.jpg)">
-		    <div class="container clearfix">
-		    	<div class="row justify-content-center">
-			        <div class="col-lg-5 col-md-6 text-center">
-			            <div class="box_1" data-cue="slideInUp">
-			                <h2>We value you</h2>
-			                <p>
-								At Wazi Business, we believe in creating meaningful connections and unforgettable experiences. As a leading integrated world-class service provider, we are driven by a purpose-led approach that revolves around helping our clients live their dreams.
+			<div class="container clearfix">
+				<div class="row justify-content-center">
+					<div class="col-lg-5 col-md-6 text-center">
+						<div class="box_1" data-cue="slideInUp">
+							<h2>Brand Promise</h2>
+							<p>
+								Memories are made of these: quality, professionalism, personal touch.
 							</p>
-			                <a href="contacts.php" class="btn_1 mt-3">Contact us</a>
-			            </div>
-			        </div>
-			    </div>
-    		</div>
-    	</div>
-   		<!--/call_section-->
+							<a href="contacts.php" class="btn_1 mt-3">Contact us</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!--/call_section-->
 
 		<!-- /pattern_2 -->
 	</main>
 	<!-- /main -->
 
 	<?php
-		include_once("includes/footer.php");
+	include_once("includes/footer.php");
 	?>
 	<!--/footer-->
 
@@ -327,17 +322,17 @@
 	</div>
 	<!-- /.modal -->
 
-	
+
 
 	<!-- COMMON SCRIPTS -->
-    <script src="js/common_scripts.min.js"></script>
-    <script src="js/slider.js"></script>
-    <script src="js/common_func.js"></script>
-    <script src="phpmailer/validate.js"></script>
+	<script src="js/common_scripts.min.js"></script>
+	<script src="js/slider.js"></script>
+	<script src="js/common_func.js"></script>
+	<script src="phpmailer/validate.js"></script>
 
-    <!-- SPECIFIC SCRIPTS (wizard form) -->
-    <script src="js/wizard/wizard_scripts.min.js"></script>
-    <script src="js/wizard/wizard_func.js"></script>
+	<!-- SPECIFIC SCRIPTS (wizard form) -->
+	<script src="js/wizard/wizard_scripts.min.js"></script>
+	<script src="js/wizard/wizard_func.js"></script>
 
 </body>
 
